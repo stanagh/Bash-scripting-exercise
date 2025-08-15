bd82d1f2c5c0bb15f380b63a80c3c5e3c28fa97b
Stanley <oderaghara5@icloud.com>
2025-08-15 20:57:45 +0100

    Level 2 completed

---
+++ Level 2/level2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {1..10}
+do
+  echo "Number: $i"
+done
+
+
+## This script uses a for loop to iterate through numbers 1 to 10 and prints each number with the prefix "Number: ".
