04ce0c7b40d2799d910cf93784d380a2366c1851
Stanley <oderaghara5@icloud.com>
2025-08-15 20:27:59 +0100

    Level 1 completed

---
+++ Level 1/level1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir Fruits
+touch banana.txt apple.txt orange.txt
+ls
+
+
+## This script creates a directory named "Fruits" and then creates three text files: banana.txt, apple.txt, and orange.txt. Finally, it lists the contents of the current directory.
